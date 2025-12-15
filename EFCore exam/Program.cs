64c9c95fedd99c47c002550782891933bec74c5c
Muffin <rostyk20049@gmail.com>
2025-12-15 20:11:02 +0200

    Add project files.

---
+++ EFCore exam/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+namespace  VinylStore
+{
+    public class Vinyl
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string BandName { get; set; }
+        public string Distributor { get; set; }
+        public int TracksNum { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public int CostForStore { get; set; }
+        public int Cost { get; set; }
+    }
+    public class VinylContext
+    {
+
+    }
+
+    public class VinylRepo
+    {
+
+    }
+}
